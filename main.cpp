f8f8718f79b7833643faa3e78d76ee31508e9123
divinojack <115471906+divinojack@users.noreply.github.com>
2022-11-16 12:08:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-   float a=0, b=0, x=0, y=0, z=0;
+    float a=0, b=0, x=0, y=0, z=0;
     cin >> a >> b >> x >> y;
     if(x<0 && y>0){
          z= a*x-b*y;
-         cout<< "Risultato=" << z << endl;
-       }
+         cout<< "z=" << z << endl;
+    }
     else if(x>=0 && y<=0){
         z=a*x*x-b*y;
-        cout<< "Risultato=" << z << endl;
+        cout<< "z=" << z << endl;
     }
     else{
         z=a*x+b*y*y;
-        cout<< "Risultato=" << z << endl;
+        cout<< "z=" << z << endl;
     }
    return 0;
 }
